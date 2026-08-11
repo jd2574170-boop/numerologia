4c4b32bf1d46b3fa66cec7a9a47b40b1ee007d87
Gregorio_12 <jd2574170@gmail.com>
2026-08-11 16:45:30 -0500

    Delete src directory

--- src/routes/compatibility.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { checkCompatibility } = require('../controllers/compatibility.controller');
-const { protect } = require('../middlewares/auth.middleware');
-
-router.post('/check', protect, checkCompatibility);
-
-module.exports = router;
--- src/routes/readings.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateReading, getHistory } = require('../controllers/readings.controller');
-const { protect } = require('../middlewares/auth.middleware');
-
-router.post('/generate', protect, generateReading);
-router.get('/history', protect, getHistory);
-
-module.exports = router;
